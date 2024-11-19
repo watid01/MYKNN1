e0d063ef94c73b6bcad42de4cca93a392d2902ef
Watid Kerdsrithong <framewatid01@gmail.com>
2024-11-19 14:58:42 +0700

    add file

--- home.py
+++ home.py
@@ -5,3 +5,8 @@ st.title("🥨🥨WedSite Developing using Python🥨🥨")
 st.header("🥪WedSite Developing using Python🥪")
 
 st.subheader("WAtid MTV")
+st.image('./img/download.jfif')
+
+dt-pd.read_scv('./data/iris.csv')
+st.header("ข้อมูลดอกไม้")
+st.write(dt.head(10))
